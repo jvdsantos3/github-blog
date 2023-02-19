51b5758aa6ae9f336105238a8311b8017f4e56b3
João <jv.dossantos012@gmail.com>
2023-02-19 00:23:04 -0300

    Finished Project

--- src/Pages/Post/styles.ts
+++ src/Pages/Post/styles.ts
@@ -14,4 +14,17 @@ export const PostContent = styled.div`
     width: 100%;
     object-fit: cover;
   }
+
+  a {
+    text-decoration: none;
+
+    border-top: 1px solid transparent;
+    border-bottom: 1px solid transparent;
+
+    color: ${(props) => props.theme.blue};
+
+    &:hover {
+      border-bottom: 1px solid ${(props) => props.theme.blue};
+    }
+  }
 `
